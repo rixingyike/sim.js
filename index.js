eeef65d5b730407d128a7572c18041a664cc261f
liyi13 <liyi13@vipkid.com.cn>
2019-09-20 18:37:15 +0800

    add networklisten

--- index.js
+++ index.js
@@ -5,15 +5,18 @@
 'use strict'
 
 let service = require('./lib/service')
-var Promise = require('./lib/bluebird-3.5.0.min')
-var qiniu = require('./lib/qiniu')
+let Promise = require('./lib/bluebird-3.5.0.min')
+let qiniu = require('./lib/qiniu')
 let page = require('./lib/page')
-var promise = {
+let promise = {
   all: Promise.all,
 }
 
-var app = {
+// serverUrlBase: "http://localhost:4000" //本地地址,"http://192.168.3.45:4000"
+// serverUrlBase: "https://api.rixingyike.com/wegame" //线上地址
+let app = {
   data: {
+    userInfo: null,//验证后的用户信息对象
     loading: false,
   },
   version: "1.0",
@@ -22,8 +25,8 @@ var app = {
   page: page,
   config: {
     showLoadingOnRequest: false //发起http请求时是否自动显示”加载中“提示
-    , enableWeappUserAutoLogin: false //启用小程序用户自动登陆服务器端,需要server端支持
-    , serverUrlBase: "http://localhost:4001" //小程序用户的登陆地址
+    , enableWeappUserAutoLogin: true //启用小程序用户自动登陆服务器端,需要server端支持
+    , serverUrlBase: "http://192.168.0.28:4000" //服务端基础地址
     , qiniu:{
       serverBase:"http://7xndm1.com1.z0.glb.clouddn.com/"
       ,uploadUrl:"https://up.qbox.me"
@@ -35,12 +38,12 @@ var app = {
 // 向服务器发起http请求
 function request(url, data, options) {
   return new Promise(function (resolve, reject) {
-    wx.showNavigationBarLoading()
+    // wx.showNavigationBarLoading()
 
     // 调用成功、失败都会执行
     let complete = function () {
       if (app.config.showLoadingOnRequest) wx.hideLoading()
-      wx.hideNavigationBarLoading()
+      // wx.hideNavigationBarLoading()
     }
 
     // 去掉微信的封装,直接返回服务器的结果
@@ -51,7 +54,7 @@ function request(url, data, options) {
         reject(res.errMsg)
       }
     }
-    var args = {
+    let args = {
       url: url,
       header: { 'Content-Type': 'json' },
       success: success,
@@ -89,11 +92,11 @@ function getUserInfo() {
       return
     }
 
-    wx.showNavigationBarLoading()
+    // wx.showNavigationBarLoading()
     let complete = function () {
-      wx.hideNavigationBarLoading()
+      // wx.hideNavigationBarLoading()
     }
-    var reject = reject || (function(err){
+    reject = reject || (function(err){
       console.log(err)
     })
 
@@ -183,16 +186,16 @@ function humanFormatTime(ms) {
 // 上传图片至七牛服务器
 function uploadToQiniu(localFile) {
   return new Promise(function (resolve, reject) {
-    wx.showNavigationBarLoading()
+    // wx.showNavigationBarLoading()
 
     // 去掉微信的封装,直接返回服务器的结果
     let success = function (res) {
-      wx.hideNavigationBarLoading()
+      // wx.hideNavigationBarLoading()
       resolve(res.imageURL)
       console.log(res)
     }
-    var reject = reject || (function (err) {
-      wx.hideNavigationBarLoading()
+    reject = reject || (function (err) {
+      // wx.hideNavigationBarLoading()
       console.log(err)
     })
     let options = {
@@ -213,11 +216,104 @@ function selectAndUploadImageToQiniu() {
       sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
       sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
       success: res => {
-        var localImgFile = res.tempFilePaths[0]
+        let localImgFile = res.tempFilePaths[0]
         uploadToQiniu(localImgFile).then(resolve).catch(reject)
       },
       fail:reject
     })
+  })
+}
+
+// 获取微信群id
+function getWXGroupID(callback) {
+  let options = wx.getLaunchOptionsSync()
+  let shareTicket = options.shareTicket
+  console.log("shareTicket", shareTicket)
+  if (!shareTicket) {
+    callback(null)
+    return
+  }
+
+  let getGid = function (res) {
+    var data = {
+      encrypted_data: res.encryptedData,
+      iv: res.iv,
+      code: res.code,
+    }
+    wx.request({
+      method: 'POST',
+      data: data,
+      url: `${app.config.serverUrlBase}/weapp/unencrypt_groupid`,
+      header: {
+        'content-type': 'application/json'
+      },
+      success: (res) => {
+        if (res.statusCode == 200 && res.errMsg == 'request:ok') {
+          console.log("unencryptgid", res.data)
+          var data = res.data
+          if (data && data.data) {
+            var openGId = data.data.openGId
+            // console.log("来源群id", openGId)
+            callback(openGId)
+          }
+        }
+      }
+    })
+  }
+  wx.getShareInfo({
+    shareTicket: shareTicket,
+    success: res => {
+      if (res.errMsg == "getShareInfo:ok") {
+        wx.login({
+          success: (res2) => {
+            res["code"] = res2.code
+            getGid(res)
+          }
+        })
+
+      }
+    }
+  })
+}
+
+// 检测网络状态
+let networkType = "unknown"
+function watchNetwork() {
+  const onNetworkTypeChanged = function (nt) {
+    if (networkType != nt) {
+      networkType = nt
+      switch (networkType) {
+        case "wifi":
+          console.log("网络状态Ok")
+          break;
+        case "2g":
+        case "3g":
+        case "4g":
+          wx.showModal({
+            title: '提示',
+            content: '当前处于移动网络，请注意流量消耗',
+            showCancel: false,
+          })
+          break
+        default:
+          wx.showModal({
+            title: '提示',
+            content: '网络不通，请检查',
+            showCancel: false,
+          })
+      }
+    }
+  }
+  wx.getNetworkType({
+    success(res) {
+      console.log("网络类型：", res.networkType)
+      onNetworkTypeChanged(res.networkType)
+    }
+  })
+  wx.onNetworkStatusChange((res) => {
+    console.log("网络类型变化为：", res.networkType)
+    onNetworkTypeChanged(res.networkType)
+    console.log(res.isConnected)
   })
 }
 
@@ -227,5 +323,7 @@ app.humanFormatTime = humanFormatTime
 app.uploadToQiniu = uploadToQiniu
 app.selectAndUploadImageToQiniu = selectAndUploadImageToQiniu
 app.page = page
+app.getWXGroupID = getWXGroupID
+app.watchNetwork = watchNetwork
 
 module.exports = app
